6721dee2985a84b7e4c6e5fa9683d051f5747d76
youkeongjun <youkeongjun@naver.com>
2022-08-10 01:32:04 +0900

    template

---
+++ 2_node.js/syntax/template.js
@@ -0,0 +1,19 @@
+var name = 'cr7sr4'
+
+var letter = 'jshdf '+name+' \n\
+\
+kmsdn'+name+'\n\
+\
+rkmsrd'+name+''
+console.log(letter)
+
+
+
+console.log("----------")
+
+
+
+var letter = `jshdf ${name}
+
+kmsdn${name} rkmsrd${1+2}`
+console.log(letter)
