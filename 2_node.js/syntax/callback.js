6d30bc82c45b8f8240c39b170568a7b64531cab6
youkeongjun <youkeongjun@naver.com>
2022-08-10 22:57:03 +0900

    add pm2

--- 2_node.js/syntax/callback.js
+++ 2_node.js/syntax/callback.js
@@ -2,6 +2,8 @@
 //     console.log('A')
 // }
 
+const { listenerCount } = require("process")
+
 var a = function(){
     console.log('B')
 }
@@ -11,3 +13,9 @@ function slowfunc(callback){
     callback()
 }
 slowfunc(a)
+
+
+// npm install pm2 -g
+// pm2 list  
+// pm2 monit 
+// pm2 start main.js
