47e85c0142d4f383a37dc07d22942fbf65c8e710
youkeongjun <youkeongjun@naver.com>
2022-08-11 03:27:49 +0900

    object & loop

--- 2_node.js/syntax/object.js
+++ 2_node.js/syntax/object.js
@@ -1,9 +1,21 @@
 var members=['cr7','sr4','pm3']
 console.log(members[1])
 
+var i = 0;
+while(i<members.length){
+    console.log('arr loop',members[i])
+    i+=1;
+}
+
 var nationality = {
     'portugal' : 'cr7',
     'spain' : 'sr4',
     'italy' : 'pm3'
 }
 console.log(nationality.portugal)
+
+
+for(var object in nationality){
+    console.log('\nnationality =>',object,
+    '\nvalue=>',nationality[object])
+}
