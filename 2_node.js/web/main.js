488b87e68d6f87fd2abef6e9ebced2a148f3f76c
youkeongjun <youkeongjun@naver.com>
2022-08-10 23:42:26 +0900

    POST request

--- 2_node.js/web/main.js
+++ 2_node.js/web/main.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
+var qs = require('querystring')
 
 function templateHTML(title,list,body){
   return `
@@ -110,6 +111,19 @@ var app = http.createServer(function(request,response){
       response.writeHead(200);
       response.end(template);
      })
+    }else if(pathname==='/create_process'){
+      var body = ''
+      request.on('data',function(data){
+        body +=data;
+      })
+      request.on('end',function(){
+        var post = qs.parse(body)
+        var title = post.title
+        var description = post.description
+        console.log(post.description)
+      })
+      response.writeHead(200);
+      response.end('success');
     }
   
      else{
