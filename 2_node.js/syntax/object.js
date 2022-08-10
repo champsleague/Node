2721c49126ee3db2e6de6fec679956cd740685ef
youkeongjun <youkeongjun@naver.com>
2022-08-11 03:20:41 +0900

    obecjt

---
+++ 2_node.js/syntax/object.js
@@ -0,0 +1,9 @@
+var members=['cr7','sr4','pm3']
+console.log(members[1])
+
+var nationality = {
+    'portugal' : 'cr7',
+    'spain' : 'sr4',
+    'italy' : 'pm3'
+}
+console.log(nationality.portugal)
