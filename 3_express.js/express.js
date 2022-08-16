f8af2b04b08d58d0bcc5b744a3398bef6a43bee2
champsleague <youkeongjun@naver.com>
2022-08-16 21:17:14 +0900

    err

--- 3_express.js/express.js
+++ 3_express.js/express.js
@@ -37,33 +37,40 @@ app.get('/',function(request,response) {
 
 
 
-app.get('/page/:pageId',function(request,response) {
+app.get('/page/:pageId',function(request,response,next) {
     fs.readdir('./data', function(error, filelist){
         var filteredId = path.parse(request.params.pageId).base;
         fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
-          var title = request.params.pageId;
-          var sanitizedTitle = sanitizeHtml(title);
-          var sanitizedDescription = sanitizeHtml(description, {
-            allowedTags:['h1']
-          });
-          var list = template.list(request.list);
-          var html = template.HTML(sanitizedTitle, list,
-            `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
-            ` <a href="/create">create</a>
-              <a href="/update/${sanitizedTitle}">update</a>
-              <form action="/delete_process" method="post">
-                <input type="hidden" name="id" value="${sanitizedTitle}">
-                <input type="submit" value="delete">
-              </form>`
-          );
-          response.send(html);
-        });
+
+        if(err){
+            next(err)
+           
+        }else{
+            var title = request.params.pageId;
+            var sanitizedTitle = sanitizeHtml(title);
+            var sanitizedDescription = sanitizeHtml(description, {
+              allowedTags:['h1']
+            });
+            var list = template.list(request.list);
+            var html = template.HTML(sanitizedTitle, list,
+              `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
+              ` <a href="/create">create</a>
+                <a href="/update/${sanitizedTitle}">update</a>
+                <form action="/delete_process" method="post">
+                  <input type="hidden" name="id" value="${sanitizedTitle}">
+                  <input type="submit" value="delete">
+                </form>`
+            );
+            response.send(html);
+        }     
+         
       });
     })
+})
 
 
 // CREATE
-app.get('/create',function(req,res){
+app.get('/create',function(request,response){
     fs.readdir('./data', function(error, filelist){
         var title = 'WEB - create';
         var list = template.list(request.list);
@@ -78,7 +85,7 @@ app.get('/create',function(req,res){
             </p>
           </form>
         `, '');
-        res.send(html);
+        response.send(html);
       });
 }) 
 
@@ -167,7 +174,17 @@ app.post('/delete_process',function(request,response){
           });
 
 
+app.use(function(req,res,next){
+    res.status(404).send("sorry, not found")
+})
 
+
+app.use((err, req, res, next) => {
+    console.error(err.stack)
+    res.status(500).send('Something broke!')
+  })
+
+  
 app.listen(3000,function(){
 console.log("app listening on port 3000")
 })
