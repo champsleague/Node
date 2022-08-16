ce5cea9a5d7a3d2ad8fe9953d0d44a0fa7c8e222
champsleague <youkeongjun@naver.com>
2022-08-16 18:19:00 +0900

    routing

--- 3_express.js/express.js
+++ 3_express.js/express.js
@@ -1,7 +1,15 @@
 const express = require('express')
 const app = express()
 
-app.get('/',(req,res)=> res.send('hi'))
+// route, routing
+// app.get('/',(req,res)=> res.send('hi'))
+app.get('/',function(req,res) {
+    return res.send('/')
+})
+
+app.get('/page',function(req,res) {
+    return res.send('/page')
+})
 
 app.listen(3000,()=> 
 console.log("app listening on port 3000"))
