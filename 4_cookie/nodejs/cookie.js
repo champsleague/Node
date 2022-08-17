3c883e64f619730865547d4f7ee381833fa5278a
champsleague <youkeongjun@naver.com>
2022-08-17 21:27:08 +0900

    install node modules

--- 4_cookie/nodejs/cookie.js
+++ 4_cookie/nodejs/cookie.js
@@ -1,4 +1,15 @@
 var http = require('http')
+var cookie = require('cookie')
 http.createServer(function(request,response){
+    console.log(request.headers.cookie)
+
+    var cookies={}
+    if(request.headers.cookie !== undefined){
+        cookies = cookie.parse(request.headers.cookie)
+    }
+
+    response.writeHead(200,{
+        'Set-Cookie' : ['yummy_cookie=choco','tasty_cookie=strawberry']
+    })
     response.end('Cookie')
 }).listen(3000)
