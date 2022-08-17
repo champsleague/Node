626333ca56dfd68308bf24280df924fc7fd05b85
champsleague <youkeongjun@naver.com>
2022-08-17 21:59:13 +0900

    path, domain

--- 4_cookie/nodejs/cookie.js
+++ 4_cookie/nodejs/cookie.js
@@ -11,7 +11,13 @@ http.createServer(function(request, response){
         'Set-Cookie':[
             'yummy_cookie=choco', 
             'tasty_cookie=strawberry',
-            `Permanent=cookies; Max-Age=${60*60*24*30}`
+            `Permanent=cookies; Max-Age=${60*60*24*30}`,
+            'Secure=Secure; Secure',
+            'HttpOnly=HttpOnly; HttpOnly',
+            'Path=Path; Path=/cookie',
+            'Domain=Domain; Domain=o2.org'
+            // o2.org라는 도메인 적게되면 (.생략되어있음)
+            // 그 앞에 어떤 서버 도메인에서도 살아남는다
         ]
     });
     response.end('Cookie!!');
